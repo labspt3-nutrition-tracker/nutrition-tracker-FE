a0a29083581524f8083e85276cc1ae704d459392
jrizza88 <jetorres88@gmail.com>
2019-05-13 23:33:30 -0400

    downloaded dependencies, made branch and started Settings.js file

---
+++ client/src/Components/Settings.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles/index';
+import Button from '@material-ui/core/Button';
+
+
+class Settings extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            'firstName': '',
+            'lastName': '',
+            // 'username': '',
+            'password': '',
+            'newPassword': '',
+            'email': '',
+            // 'calorieGoal': '',
+            // 'weight': ''
+        }
+    }
+    
+    componentDidMount() {
+
+    }
+
+    handleChange(e){
+        this.setState({ [e.target.name]: e.target.value})
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+
+    }
+
+    render(){
+        return (
+            <>
+                <form>
+
+                </form>
+            </>
+        )
+    }
+
+}
+
+export default Settings;
