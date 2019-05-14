adfb1cde75266fbbb0189ff5123a1e684ab24f8f
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-05-13 21:55:18 -0700

    Add Meyer reset to App.css, add other styles to Home

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -4,32 +4,42 @@ import Button from '@material-ui/core/Button';
 
 const Home = () => (
   <div className="home">
-    <header>
-      <div>Nutrition Buddy</div>
-      <div>
-        <Button>About Us</Button>
-        <Button>Plans</Button>
-        <Button>Get Started</Button>
-      </div>
-    </header>
-    <main>
-      <div>Quick, easy food tracking to help you accomplish your health goals</div>
-      <div>Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies.</div>
-      <div className="features-con">
-        <div className="feature-box">
-          <div className="feature-img"></div>
-          <div className="feature-text">Lorem Ipsum</div>
+    <div className="container">
+      <header className="header-home">
+        <div className="logo">Nutrition Buddy</div>
+        <div>
+          <Button>About Us</Button>
+          <Button>Plans</Button>
+          <Button>Get Started</Button>
         </div>
-        <div className="feature-box">
-          <div className="feature-img"></div>
-          <div className="feature-text">Lorem Ipsum</div>
+      </header>
+      <main>
+        <div className="home-text">
+          <div className="tagline">Quick, easy food tracking to help you accomplish your health goals</div>
+          <div className="detail">Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies.</div>
         </div>
-        <div className="feature-box">
-          <div className="feature-img"></div>
-          <div className="feature-text">Lorem Ipsum</div>
+        <div className="features-con">
+          <div className="feature-box">
+            <div className="feature-img">
+              <img src="http://lorempixel.com/200/200/food" />
+            </div>
+            <div className="feature-text">Lorem Ipsum</div>
+          </div>
+          <div className="feature-box">
+            <div className="feature-img">
+              <img src="http://lorempixel.com/200/200/technics" />
+            </div>
+            <div className="feature-text">Lorem Ipsum</div>
+          </div>
+          <div className="feature-box">
+            <div className="feature-img">
+              <img src="http://lorempixel.com/200/200/people" />
+            </div>
+            <div className="feature-text">Lorem Ipsum</div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   </div>
 )
 
