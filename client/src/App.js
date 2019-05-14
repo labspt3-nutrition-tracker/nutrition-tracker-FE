27d3b1868b5714bd8d99f58ca61a04acd93a330b
Alando Appling <alandoappling@gmail.com>
2019-05-13 21:49:25 -0700

    Started working on settings page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './Components/Login';
+import Setting from './Components/Settings';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Setting/>
       <div></div>
     </div>
   );
