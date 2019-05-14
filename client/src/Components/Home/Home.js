2269d611f21d190207aa772f4cfca22006c09e7c
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-05-13 21:34:16 -0700

    Addpla ceholder images and text to Home page along with styles.

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import Header from '../Reusables/Header';
+import Button from '@material-ui/core/Button';
+
 
 const Home = () => (
-  <div>
-    <Header></Header>
+  <div className="home">
+    <header>
+      <div>Nutrition Buddy</div>
+      <div>
+        <Button>About Us</Button>
+        <Button>Plans</Button>
+        <Button>Get Started</Button>
+      </div>
+    </header>
+    <main>
+      <div>Quick, easy food tracking to help you accomplish your health goals</div>
+      <div>Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies.</div>
+      <div className="features-con">
+        <div className="feature-box">
+          <div className="feature-img"></div>
+          <div className="feature-text">Lorem Ipsum</div>
+        </div>
+        <div className="feature-box">
+          <div className="feature-img"></div>
+          <div className="feature-text">Lorem Ipsum</div>
+        </div>
+        <div className="feature-box">
+          <div className="feature-img"></div>
+          <div className="feature-text">Lorem Ipsum</div>
+        </div>
+      </div>
+    </main>
   </div>
 )
 
