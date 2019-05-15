afff315f164f843071a4c6ee58076d4a3116f278
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-05-14 19:33:41 -0700

    Add Calories component to Dashboard; work on Mui styles.

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Header from '../Reusables/Header';
+import Calories from './Calories';
 
 const Dashboard = () => (
   <div>
     <Header></Header>
+    <div className="dash-title">Today's Food Entries</div>
+    <div className="current-entries">
+      <div>Breakfast</div>
+      <div>Lunch</div>
+      <div>Dinner</div>
+      <div>Snack</div>
+    </div>
+    <div className="entry-form"><input type="text" placeholder="Add food here..."/></div>
+    <Calories />
+
   </div>
 )
 
