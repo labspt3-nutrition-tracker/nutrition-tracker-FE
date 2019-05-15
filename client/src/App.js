e1ecfd145155717936d652c717ad8cad0e21763d
jrizza88 <jetorres88@gmail.com>
2019-05-14 20:25:15 -0400

    completed some styling for Billing.js and started stats.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Route, Link} from 'react-router-dom';
 import './App.css';
 
-import Billing from './Components/Billing';
+import Billing from './Components/Billing/Billing';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <div>
--- client/src/Components/Billing/Billing.js
+++ client/src/Components/Billing/Billing.js
@@ -2,6 +2,18 @@ import React from 'react';
 import {withStyles} from '@material-ui/core/styles/index';
 import Button from '@material-ui/core/Button';
 
+const BillingSubmitButton = withStyles({
+    root: {
+        color: '#FCFCFB',
+        backgroundColor: '#F4B4C3',
+        width: '200px',
+        height: 90,
+        '&:hover': {
+            backgroundColor: '#dba2af'
+        }
+    }
+})(Button);
+
 
 class Billing extends React.Component{
     constructor(props){
@@ -22,14 +34,14 @@ class Billing extends React.Component{
 
     handleChange = (e) => {
         console.log(e.target.value)
-        this.setState({ ...this.state, [e.target.name]: e.target.value})
+        console.log(e)
+        this.setState({ [e.target.name]: e.target.value})
     }
 
     handleSubmit = (e) => {
         console.log(e)
         e.preventDefault();
-console.log('handle submit');
-const { firstName, lastName, email } = this.state;
+// const { firstName, lastName, email } = this.state;
 
     }
 
@@ -40,28 +52,32 @@ const { firstName, lastName, email } = this.state;
                     <input 
                         type="text"
                         name="firstName"
-                        placeholder={this.state.firstName}
-                        handleChange={this.handleChange}
+                        placeholder="First Name"
+                        onChange={this.handleChange}
                         value={this.state.firstName}
                     />
                      <input 
                         type="text"
                         name="lastName"
-                        placeholder={this.state.lastName}
-                        handleChange={this.handleChange}
+                        placeholder="Last Name"
+                        onChange={this.handleChange}
                         value={this.state.lastName}
                     />
                      <input 
                         type="text"
                         name="email"
-                        placeholder={this.state.email}
-                        handleChange={this.handleChange}
+                        placeholder="Email"
+                        onChange={this.handleChange}
                         value={this.state.email}
                     />
                 </form>
-                <Button variant="contained" type="submit "color="primary">
+                <BillingSubmitButton variant="contained" type="submit" size="large">
+                    Buy Now
+                </BillingSubmitButton>
+                {/* <Button variant="contained" type="submit" color="#3685B5" size="large">
                 Submit changes
-                </Button>
+                </Button> */}
+
             </>
         )
     }
