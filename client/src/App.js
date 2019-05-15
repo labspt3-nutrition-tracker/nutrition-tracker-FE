8b567f66a748e415b5908e694c6ef61f41812e8c
Alando Appling <alandoappling@gmail.com>
2019-05-14 20:01:33 -0700

    Added routes to login and setting component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Settings from './Components/Settings';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Home from './Components/Home/Home';
 
@@ -10,6 +12,8 @@ function App() {
       <main>
         <Route exact path="/" component={Home} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/settings" component={Settings} />
       </main>
     </div>
   );
