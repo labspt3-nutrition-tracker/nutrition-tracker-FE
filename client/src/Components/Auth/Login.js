8080a9a8febe687468e2b52b2575139a4a89dded
Alando Appling <alandoappling@gmail.com>
2019-05-29 18:24:52 -0700

    Minor changes made to Login Component

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import LoginForm from "./LoginForm";
 import { Redirect } from 'react-router-dom'
@@ -5,142 +6,169 @@ import { GoogleLogin } from 'react-google-login';
 import styled from "styled-components";
 import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
-import { Mutation, Query } from 'react-apollo';
+
 
 
 
 
 
 const LoginOrRegisterContainer = styled.div`
- background: #fcfcfb;
- display: flex;
- justify-content: center;
- align-content: center;
- flex-wrap: wrap;
- width: 100%;
- height: 100vh;
+  background: #fcfcfb;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100vh;
 `;
 
 const LoginOrRegisterForm = styled.div`
- background: #3685b5;
- width: 50%;
- height: 500px;
- padding: 100px;
- display: flex;
- justify-content: center;
- align-content:center;
- flex-wrap: wrap;
- -webkit-box-shadow: 6px 7px 24px -1px rgba(0, 0, 0, 0.75);
- -moz-box-shadow: 6px 7px 24px -1px rgba(0, 0, 0, 0.75);
- box-shadow: 6px 7px 24px -1px rgba(0, 0, 0, 0.75);
- border-radius: 10px;
+  background: #3685b5;
+  width: 50%;
+  height: 500px;
+  padding: 100px;
+  display: flex;
+  justify-content: center;
+  align-content:center;
+  flex-wrap: wrap;
+  -webkit-box-shadow: 6px 7px 24px -1px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 6px 7px 24px -1px rgba(0, 0, 0, 0.75);
+  box-shadow: 6px 7px 24px -1px rgba(0, 0, 0, 0.75);
+  border-radius: 10px;
 `;
 
 const FormContainer = styled.div`
-   display:flex;
-   justify-content:center;
-   width:50%;
+    display:flex;
+    justify-content:center;
+    width:50%;
 `;
 
 const ADD_USER = gql`
- mutation addUser($input: UserInput!){
-   addUser(input: $input){
-     id
-   }
- }
+  mutation addUser($input: UserInput!){
+    addUser(input: $input){
+      id
+    }
+  }
 `;
 
 const USER_EXIST = gql`
- query getUserBy($param: String!, $value: String!){
-   getUserBy(param: $param, value: $value){
-     email
-   }
- }
+  query getUserBy($param: String!, $value: String!){
+    getUserBy(param: $param, value: $value){
+      email
+    }
+  }
 `;
+
+const GET_CURRENT = gql`
+  query getCurrentUser{
+    getCurrentUser{
+      id
+      email
+    }
+  }
+`
 
 class LoginOrRegister extends React.Component {
 
- constructor(props){
-   super(props);
-   this.state = {
-     toDashboard: false,
-     checkExistence: false,
-     firstName: "",
-     lastName: "",
-     username: "",
-     email: "",
-     userType: "",
-     calorieGoal:0,
-     weight:0
-   }
+  constructor(props){
+    super(props);
+    this.state = {
+      toDashboard: false,
+      checkExistence: false,
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      userType: "",
+      calorieGoal:0,
+      weight:0
+    }
 
- }
- onSuccess = async googleUser => {
-   console.log(googleUser.profileObj.email)
-   const email = googleUser.profileObj.email;
-   const idToken = googleUser.getAuthResponse().id_token;
-   localStorage.setItem('token', idToken);
+  }
+  onSuccess = async googleUser => {
+    console.log(googleUser.profileObj.email)
+    const email = googleUser.profileObj.email;
+    const idToken = googleUser.getAuthResponse().id_token;
+    localStorage.setItem('token', idToken);
 
+    const test = localStorage.getItem('token')
 
+    this.getCurrentUser(test);
 
-   const client = new ApolloClient({
-     uri:'https://nutrition-tracker-be.herokuapp.com',
-     headers: {authorization: idToken}
-   })
+    const client = new ApolloClient({
+      uri:'https://nutrition-tracker-be.herokuapp.com',
+      headers: {authorization: idToken}
+    })
 
-   client.query({
-     query: USER_EXIST,
-     variables: {
-       param: "email",
-       value: email
-     }
-   }).then(response => {
-     this.setState({toDashboard: !this.state.toDashboard})
-   }).catch(err => this.setState({checkExistence: !this.state.checkExistence}))
- }
+    client
+      .query({
+        query: USER_EXIST,
+        variables: {
+          param: "email",
+          value: email
+        }
+      })
+      .then(response => {
+        if (response.data.getUserBy) this.setState({ toDashboard: !this.state.toDashboard });
+        else this.setState({ checkExistence: !this.state.checkExistence });
+      })
+      .catch(err => console.log(err));
+  }
 
- handleChange = (label, value) => {
-   this.setState({
-     [label]: value
-   })
- }
+  handleChange = (label, value) => {
+    this.setState({
+      [label]: value
+    })
+  }
 
- createUser = userObj => {
-   const client = new ApolloClient({
-     uri:'https://nutrition-tracker-be.herokuapp.com'
-   })
+  createUser = userObj => {
+    const client = new ApolloClient({
+      uri:'https://nutrition-tracker-be.herokuapp.com'
+    })
 
-   client.mutate({
-     mutation: ADD_USER,
-     variables: {
-       input: userObj
-     }
-   }).then(response => console.log(response.data))
- }
- render(){
-   if (this.state.toDashboard === true){
-     return <Redirect to="/dashboard"/>
-   }
-   return (
-     <LoginOrRegisterContainer>
-       <FormContainer>
-           <LoginOrRegisterForm>
-             <div>
-               {
-                 this.state.checkExistence ? ( <LoginForm addUser={this.createUser} handleChange={this.handleChange} props={this.state}/>
-                   ):(
-                     <GoogleLogin
-                     style={{height:10}}
-                     clientId='1047286164516-jv47gpee2568sc3bindc9ra3vua101t3.apps.googleusercontent.com'
-                     onSuccess={this.onSuccess}
-                     /> )
-               }
-             </div>
-           </LoginOrRegisterForm>
-       </FormContainer>
-     </LoginOrRegisterContainer>
-   );
- }
+    client.mutate({
+      mutation: ADD_USER,
+      variables: {
+        input: userObj
+      }
+    }).then(response => console.log(response.data))
+  }
+
+  getCurrentUser = idToken => {
+    const client = new ApolloClient({
+      uri:'https://nutrition-tracker-be.herokuapp.com',
+      headers: {authorization: idToken}
+    })
+
+    client.query({
+      query: GET_CURRENT
+    }).then(response => console.log(response.data) ).catch(err => console.log(err))
+
+  }
+  render(){
+    if (this.state.toDashboard === true){
+      return <Redirect to="/dashboard"/>
+    }
+    return (
+      <LoginOrRegisterContainer>
+        <FormContainer>
+            <LoginOrRegisterForm>
+              <div>
+                {
+                  this.state.checkExistence ? ( <LoginForm addUser={this.createUser} handleChange={this.handleChange} props={this.state}/>
+                    ):(
+                      <GoogleLogin
+                      style={{height:10}}
+                      clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
+                      onSuccess={this.onSuccess}
+                      /> )
+                }
+              </div>
+            </LoginOrRegisterForm>
+        </FormContainer>
+      </LoginOrRegisterContainer>
+    );
+  }
 }
 
 export default LoginOrRegister;
