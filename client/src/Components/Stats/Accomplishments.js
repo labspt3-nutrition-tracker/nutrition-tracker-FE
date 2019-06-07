c63577355e6b631aa78107dc1828639eea0b2104
Leila <nb.leila10@gmail.com>
2019-06-07 16:14:55 -0500

    Add no calories entry in warnings

--- client/src/Components/Stats/Accomplishments.js
+++ client/src/Components/Stats/Accomplishments.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -30,9 +31,10 @@ class Accomplishments extends Component {
     days = days.map(day => moment(day).format("MM/DD"));
     const success = goals.map((goal, i) => (goal === 1 ? days[i] : goal)).filter(day => day !== 0 && day !== -1);
     const warnings = goals.map((goal, i) => (goal === -1 ? days[i] : goal)).filter(day => day !== 0 && day !== 1);
+    const noEntries = goals.map((goal, i) => (goal === 0 ? days[i] : goal)).filter(day => day !== -1 && day !== 1);
     return (
       <div className={classes.root}>
-        <h2 className={classes.title}>
+        <h2 className={classes.week}>
           Last 7 days: {days[0]} - {days[6]}
         </h2>
         <Grid container justify='center' alignItems='center'>
@@ -42,12 +44,7 @@ class Accomplishments extends Component {
                 <Typography className={classes.title} variant='h5' component='h2'>
                   GOALS REACHED
                 </Typography>
-                <Typography color='textSecondary' className={classes.category}>
-                  WEIGHT
-                </Typography>
-                <Typography className={classes.pos} variant='body2' component='p'>
-                  You have lost 3 pounds.
-                </Typography>
+
                 {success.length > 0 && (
                   <>
                     <Typography color='textSecondary' className={classes.category}>
@@ -56,16 +53,20 @@ class Accomplishments extends Component {
                     <Typography className={classes.pos} variant='body2' component='p'>
                       You have stayed under your calories goal of {currentUser.calorieGoal} on these days:
                     </Typography>
-                    <ul>
+                    <ul className={classes.list}>
                       {success.map((day, i) => (
-                        <li key={i}>{moment(day).format("dddd MMM DD")}</li>
+                        <li className={classes.listItem} key={i}>
+                          {moment(day).format("dddd MMM DD")}
+                        </li>
                       ))}
                     </ul>
                   </>
                 )}
               </CardContent>
               <CardActions>
-                <Button size='medium'>Update Goals</Button>
+                <Link className={classes.link} to={"/settings"}>
+                  Update your settings
+                </Link>
               </CardActions>
             </Card>
           </Grid>
@@ -82,47 +83,75 @@ class Accomplishments extends Component {
                   You exercised every day. You burned 2500 kcal.
                 </Typography>
                 <Typography color='textSecondary' className={classes.category}>
-                  WATER
+                  WEIGHT
                 </Typography>
                 <Typography className={classes.pos} variant='body2' component='p'>
-                  You consumed the recommended 11 cups of water.
+                  You have lost 3 pounds.
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size='medium'>Update Goals</Button>
+                <Link className={classes.link} to={"/settings"}>
+                  Update your settings
+                </Link>
               </CardActions>
             </Card>
           </Grid>
           <Grid item xs={12}>
             <Card className={classes.card}>
               <CardContent>
-                <Typography className={classes.title} variant='h5' component='h2'>
+                <Typography
+                  className={classes.title}
+                  variant='h5'
+                  component='h2'
+                  style={{ borderBottom: "2px solid #F4B4C3" }}
+                >
                   WARNINGS
-                </Typography>
-                <Typography color='textSecondary' className={classes.category}>
-                  CALORIES
                 </Typography>
                 {warnings.length > 0 && (
                   <>
+                    <Typography color='textSecondary' className={classes.category}>
+                      CALORIES
+                    </Typography>
                     <Typography className={classes.pos} variant='body2' component='p'>
                       You consumed more that your daily goal of calories ({currentUser.calorieGoal}) on these days:
                     </Typography>
-                    <ul>
+                    <ul className={classes.list}>
                       {warnings.map((day, i) => (
-                        <li key={i}>{moment(day).format("dddd MMM DD")}</li>
+                        <li className={classes.listItem} key={i}>
+                          {moment(day).format("dddd MMM DD")}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+                {noEntries.length > 0 && (
+                  <>
+                    <Typography color='textSecondary' className={classes.category}>
+                      No calories Entry
+                    </Typography>
+                    <Typography className={classes.pos} variant='body2' component='p'>
+                      You didn't record any food entry on these days:
+                    </Typography>
+                    <ul className={classes.list}>
+                      {noEntries.map((day, i) => (
+                        <li className={classes.listItem} key={i}>
+                          {moment(day).format("dddd MMM DD")}
+                        </li>
                       ))}
                     </ul>
                   </>
                 )}
                 <Typography color='textSecondary' className={classes.category}>
-                  PROTEINS
+                  No Exercise Entry
                 </Typography>
                 <Typography className={classes.pos} variant='body2' component='p'>
-                  You need to eat more proteins during the day.
+                  You didn't record any exercise entry on these days:
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size='medium'>Update Goals</Button>
+                <Link className={classes.link} to={"/settings"}>
+                  Update your settings
+                </Link>
               </CardActions>
             </Card>
           </Grid>
@@ -135,12 +164,12 @@ class Accomplishments extends Component {
 const styles = theme => ({
   root: {
     width: "100%",
-    margin: "50px auto",
+    margin: "20px auto",
     maxWidth: "1200px",
     padding: "20px"
   },
   card: {
-    height: "500px",
+    minHeight: "250px",
     margin: "10px",
     display: "flex",
     flexDirection: "column",
@@ -151,19 +180,40 @@ const styles = theme => ({
     margin: "0 2px",
     transform: "scale(0.8)"
   },
+  week: {
+    fontSize: "2rem",
+    marginBottom: 20,
+    color: "#3685B5",
+    textAlign: "center"
+  },
   title: {
     fontSize: "2rem",
     marginBottom: 20,
-    color: "#2196F3",
-    textAlign: "center"
+    color: "#3685B5",
+    textAlign: "center",
+    paddingBottom: 5,
+    borderBottom: "2px solid #3685B5"
   },
   category: {
     fontSize: "1.7rem",
-    color: "#2196F3"
+    color: "#3685B5",
+    marginTop: 10
   },
   pos: {
     fontSize: "1.5rem",
     marginBottom: 12
+  },
+  list: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+  listItem: {
+    margin: 10
+  },
+  link: {
+    textDecoration: "none",
+    color: "#3685B5"
   }
 });
 
