976ff3868b6c2e7f2aad3b66868ab41b684278a4
david-chua <dp_chua@hotmail.com>
2019-06-11 00:52:28 -0400

    working on both api and nonapi add entry

--- client/src/Components/Dashboard/EntryForm.js
+++ client/src/Components/Dashboard/EntryForm.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 // import { Query } from "react-apollo";
 import ApolloClient from "apollo-boost";
-import { ADD_FOOD, ADD_FOOD_ENTRY } from "../../graphql/mutations";
+import { ADD_FOOD } from "../../graphql/mutations";
 import { GET_ALL_FOOD } from "../../graphql/queries";
 import gql from "graphql-tag";
 
@@ -186,7 +186,6 @@ class EntryForm extends Component {
         })
         .then( response => {
           const foodId = response[0].id;
-          console.log('foodId', foodId)
           const entryAddedToDB = {
             date: this.state.newAddFood.date,
             food_id:  parseInt(foodId),
@@ -194,31 +193,47 @@ class EntryForm extends Component {
             servingQty: this.state.newAddFood.servingQty,
             meal_category_id: parseInt(this.state.newAddFood.meal_category_id)
           };
-          client
-            .mutate({
-              mutation: ADD_FOOD_ENTRY,
-              variables: {
-                input: entryAddedToDB
-              }
-            })
-            .then(response => {
-              console.log(response);
-              this.setState({
-                errors: [],
-                edamamExist: false,
-                newAddFood: {
-                  foodName: "",
-                  caloriesPerServ: null,
-                  fats: null,
-                  carbs: null,
-                  proteins: null,
-                  edamam_id: null,
-                  meal_category_id: null,
-                  date: "",
-                  servingQty: null
-                }
-              });
-            });
+          // client
+          //   .mutate({
+          //     mutation: ADD_FOOD_ENTRY,
+          //     variables: {
+          //       input: entryAddedToDB
+          //     }
+          //   })
+          this.props.addFoodEntry(entryAddedToDB)
+          this.setState({
+            errors: [],
+            edamamExist: false,
+            newAddFood: {
+              foodName: "",
+              caloriesPerServ: null,
+              fats: null,
+              carbs: null,
+              proteins: null,
+              edamam_id: null,
+              meal_category_id: null,
+              date: "",
+              servingQty: null
+            }
+          });
+            // .then(response => {
+            //   console.log(response);
+            //   this.setState({
+            //     errors: [],
+            //     edamamExist: false,
+            //     newAddFood: {
+            //       foodName: "",
+            //       caloriesPerServ: null,
+            //       fats: null,
+            //       carbs: null,
+            //       proteins: null,
+            //       edamam_id: null,
+            //       meal_category_id: null,
+            //       date: "",
+            //       servingQty: null
+            //     }
+            //   });
+            // });
 
           // console.log("response:", response);
           // console.log("currentUser:", this.state.newAddFood.user_id);
--- client/src/Components/Dashboard/ExerEntry.js
+++ client/src/Components/Dashboard/ExerEntry.js
@@ -1,8 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// import { Query } from "react-apollo";
-import ApolloClient from "apollo-boost";
-import { EXER_QUERY, GET_CURRENT_USERID } from "../../graphql/queries";
 
 const ExerciseActivity = styled.div`
   padding: 10px;
