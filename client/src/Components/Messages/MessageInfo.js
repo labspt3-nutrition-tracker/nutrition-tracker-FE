233051c23d0cf6edcc330b397af8afb03589a142
Leila Berrouayel <nb.leila10@gmail.com>
2019-06-25 09:21:23 -0500

    Sort messages by date

--- client/src/Components/Messages/MessageInfo.js
+++ client/src/Components/Messages/MessageInfo.js
@@ -6,6 +6,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import { withStyles } from "@material-ui/core/styles";
+import * as moment from "moment";
 
 const styles = theme => ({
   text: {
@@ -16,10 +17,13 @@ const styles = theme => ({
     fontSize: "2.2rem"
   }
 });
-
 const MessageInfo = props => {
   const { sender, messages, classes } = props;
   const messagesArr = messages.filter(message => message.sender.id === sender.id && message.type === "text");
+  messagesArr.sort((a, b) => {
+    if( moment(new Date(a.created_at)).isSameOrBefore(new Date(b.created_at))) return 1 
+    else return -1;
+  })
 
   return (
     <>
