d46b29d48201c8958d8401485b0be7cf7b259d32
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-06-26 14:32:28 -0700

    Styles added to Dashboard components, fix login/logout font size

--- client/src/Components/Dashboard/Exercise.js
+++ client/src/Components/Dashboard/Exercise.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import ExerForm from "./ExerForm";
 import Typography from "@material-ui/core/Typography";
-
-
+import { withStyles } from "@material-ui/core/styles";
 
 class Exercise extends Component {
   render() {
     return (
       <ExerciseContainer>
         <div className="container">
-        <Typography variant="h4">Add exercise entry</Typography>
           <InfoCon>
             <ExerForm
               closeExerEntry={this.props.closeExerEntry}
@@ -24,24 +22,18 @@ class Exercise extends Component {
 }
 
 const ExerciseContainer = styled.div`
-  width: 30%;
-  padding: 20px;
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
   @media(max-width: 800px) {
     max-width: 500px;
     width: 100%;
   }
 `;
 
-const ExerTitle = styled.h1`
-  font-size: 1.5em;
-  font-weight: bold;
-  padding-bottom: 30px;
-  text-align: center;
-  color: blue;
-`;
-
 const InfoCon = styled.div`
   display: flex;
   justify-content: center;
 `;
+
 export default Exercise;
