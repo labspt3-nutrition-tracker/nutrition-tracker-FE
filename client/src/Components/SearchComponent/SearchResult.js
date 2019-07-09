42892859913b1d4665180355a303f514ae3a40ba
jrizza88 <jetorres88@gmail.com>
2019-07-08 20:43:23 -0400

    styling updates

--- client/src/Components/SearchComponent/SearchResult.js
+++ client/src/Components/SearchComponent/SearchResult.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Button = styled.button`
   width: 200px;
   height: 30px;
-  background: #5E366A;
+  background: #000;
   color: #FFF;
   display: block;
 
